1e46af568ea81c4e7cab1be121d34ecfd21de827
Richard Caetano <caetano@gmail.com>
2023-07-19 14:16:30 +0200

    added a team score

--- tictactoe.js
+++ tictactoe.js
@@ -1,57 +1,108 @@
 // tictactoe.js
 
+// add scores
+let scoreX = 0;
+let scoreO = 0;
+
 let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
-let currentPlayer = 'X';
+let currentPlayer = "X";
 
 function makeMove(row, column) {
-    if (board[row][column] !== '') {
-        // Cell is already filled.
-        return;
+  if (board[row][column] !== "") {
+    // Cell is already filled.
+    return;
+  }
+  board[row][column] = currentPlayer;
+  document.getElementById("tic-tac-toe-board").children[row].children[
+    column
+  ].textContent = currentPlayer;
+  if (checkWinner()) {
+    // update and display scores
+    if (currentPlayer === "X") {
+      scoreX++;
+    } else {
+      scoreO++;
     }
-    board[row][column] = currentPlayer;
-    document.getElementById('tic-tac-toe-board').children[row].children[column].textContent = currentPlayer;
-    if (checkWinner()) {
-        alert(currentPlayer + ' is the winner!');
-        return;
-    }
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    updateScores();
+    alert(currentPlayer + " is the winner!");
+    return;
+  }
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+function updateScores() {
+  document.getElementById("scores").textContent = `X: ${scoreX} - O: ${scoreO}`;
 }
 
 function checkWinner() {
-    const winningCombinations = [
-        [[0, 0], [0, 1], [0, 2]],
-        [[1, 0], [1, 1], [1, 2]],
-        [[2, 0], [2, 1], [2, 2]],
-        [[0, 0], [1, 0], [2, 0]],
-        [[0, 1], [1, 1], [2, 1]],
-        [[0, 2], [1, 2], [2, 2]],
-        [[0, 0], [1, 1], [2, 2]],
-        [[0, 2], [1, 1], [2, 0]]
-    ];
-    for (const combination of winningCombinations) {
-        if (board[combination[0][0]][combination[0][1]] === currentPlayer &&
-            board[combination[1][0]][combination[1][1]] === currentPlayer &&
-            board[combination[2][0]][combination[2][1]] === currentPlayer) {
-            return true;
-        }
+  const winningCombinations = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
+  for (const combination of winningCombinations) {
+    if (
+      board[combination[0][0]][combination[0][1]] === currentPlayer &&
+      board[combination[1][0]][combination[1][1]] === currentPlayer &&
+      board[combination[2][0]][combination[2][1]] === currentPlayer
+    ) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function resetBoard() {
-    board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-    currentPlayer = 'X';
-    const cells = document.getElementsByClassName('cell');
-    for (const cell of cells) {
-        cell.textContent = '';
-    }
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  currentPlayer = "X";
+  const cells = document.getElementsByClassName("cell");
+  for (const cell of cells) {
+    cell.textContent = "";
+  }
 }
